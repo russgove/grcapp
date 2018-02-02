d15e060f322985d5ded24b1fd052c99b872f837d
unknown <trwg1@STMF096K32.Global.Tronox.Com>
2018-02-01 20:06:50 -0500

    high risk

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -1,13 +1,11 @@
 import { find, clone, map } from "lodash";
-import { Web } from "sp-pnp-js";
-
+import pnp, { Web, List, ListAddResult } from "sp-pnp-js";
+import { HttpClient, IHttpClientOptions, HttpClientResponse } from '@microsoft/sp-http';
 export function extractColumnHeaders(headerRow: Array<String>): String[] {
-
     let headings: Array<String> = [];
     for (let header of headerRow) {
         headings.push(header.replace(/\s/g, "")); // remove spaces
     }
-
     return headings;
 }
 export class CachedId {
@@ -15,7 +13,6 @@ export class CachedId {
     public id: number | null;
 }
 export async function findId(web: Web, cachedIds: Array<CachedId>, upn: string): Promise<number | null> {
-
     let id: number | null = null;
     let cached: CachedId = find(cachedIds, (cachedId) => { return cachedId.upn === upn; });
     if (cached) {
@@ -33,7 +30,6 @@ export async function findId(web: Web, cachedIds: Array<CachedId>, upn: string):
         });
     return id;
 }
-
 export async function esnureUsers(web: Web, cachedIds: Array<CachedId>,
     data: Array<any>, columnName: string, addMessage: (message: string) => void): Promise<any> {
     let rowNumber = 0;
@@ -44,29 +40,236 @@ export async function esnureUsers(web: Web, cachedIds: Array<CachedId>,
             addMessage(`Approver  ${row.ApproverEmail} on row ${rowNumber} of the Role To Transaction File is invalid`);
         }
     }
-
     return Promise.resolve();
 }
-export function  uploadFile(web:Web,libraryName:string,file:File,saveAsFileName:string,addMessage: (message: string) => void): Promise<any> {
+export function uploadFile(web: Web, libraryName: string, file: File, saveAsFileName: string, addMessage: (message: string) => void): Promise<any> {
     debugger;
     return web.lists.getByTitle(libraryName).rootFolder.files
-      .addChunked(saveAsFileName, this.state.file, data => {
-        addMessage(`(Stage ${data.stage}) Uploaded block ${data.blockNumber} of ${data.totalBlocks}`);
-
-      }, true)
-      .then((results) => {
-        return results.file.getItem().then(item => {
-          return item.update({ Title: saveAsFileName }).then((r) => {
-            return;
-          }).catch((err) => {
-            debugger;
+        .addChunked(saveAsFileName, this.state.file, data => {
+            addMessage(`(Stage ${data.stage}) Uploaded block ${data.blockNumber} of ${data.totalBlocks}`);
+        }, true)
+        .then((results) => {
+            return results.file.getItem().then(item => {
+                return item.update({ Title: saveAsFileName }).then((r) => {
+                    return;
+                }).catch((err) => {
+                    debugger;
+                    addMessage(err.data.responseBody["odata.error"].message.value);
+                    console.log(err);
+                });
+            });
+        })
+        .catch((err) => {
             addMessage(err.data.responseBody["odata.error"].message.value);
             console.log(err);
-          });
         });
-      })
-      .catch((err) => {
-        addMessage(err.data.responseBody["odata.error"].message.value);
-           console.log(err);
-      });
-  }
+}
+export async function cleanupHomePage(webRelativeUrl: string, homePageUrl, webPartXml: string) {
+    const clientContext: SP.ClientContext = new SP.ClientContext(webRelativeUrl);
+    var oFile = clientContext.get_web().getFileByServerRelativeUrl(homePageUrl);
+    var limitedWebPartManager = oFile.getLimitedWebPartManager(SP.WebParts.PersonalizationScope.shared);
+    let webparts = limitedWebPartManager.get_webParts();
+    clientContext.load(webparts, 'Include(WebPart)');
+    clientContext.load(limitedWebPartManager);
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+    let count = webparts.get_count();
+    for (let i = 0; i < count; i++) {
+        let originalWebPartDef = webparts.get_item(i);
+        originalWebPartDef.deleteWebPart();
+    }
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            console.log("the webpartw were deleted hidden");
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+    let oWebPartDefinition = limitedWebPartManager.importWebPart(webPartXml);
+    let oWebPart = oWebPartDefinition.get_webPart();
+    limitedWebPartManager.addWebPart(oWebPart, 'Main', 1);
+    clientContext.load(oWebPart);
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            console.log("the new webpart was added");
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+}
+export async function setWebToUseSharedNavigation(webRelativeUrl: string) {
+    const clientContext: SP.ClientContext = new SP.ClientContext(webRelativeUrl);
+    var currentWeb = clientContext.get_web();
+    var navigation = currentWeb.get_navigation();
+    navigation.set_useShared(true);
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            console.log("the web was set to use shared navigation");
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+}
+export async function AddQuickLaunchItem(webUrl: string, title: string, url: string, isExternal: boolean) {
+    let nnci: SP.NavigationNodeCreationInformation = new SP.NavigationNodeCreationInformation();
+    nnci.set_title(title);
+    nnci.set_url(url);
+    nnci.set_isExternal(isExternal);
+    const clientContext: SP.ClientContext = new SP.ClientContext(webUrl);
+    const web = clientContext.get_web();
+    web.get_navigation().get_quickLaunch().add(nnci);
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+}
+export async function RemoveQuickLaunchItem(webUrl: string, titlesToRemove: string[]) {
+    const clientContext: SP.ClientContext = new SP.ClientContext(webUrl);
+    const ql: SP.NavigationNodeCollection = clientContext.get_web().get_navigation().get_quickLaunch();
+    clientContext.load(ql);
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+    debugger;
+    let itemsToDelete = [];
+    let itemCount = ql.get_count();
+    for (let x = 0; x < itemCount; x++) {
+        let item = ql.getItemAtIndex(x);
+        let itemtitle = item.get_title();
+        if (titlesToRemove.indexOf(itemtitle) !== -1) {
+            itemsToDelete.push(item);
+        }
+    }
+    for (let item of itemsToDelete) {
+        item.deleteObject();
+    }
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+    debugger;
+}
+export async function fixUpLeftNav(webUrl: string, homeUrl: string) {
+    debugger;
+    await AddQuickLaunchItem(webUrl, "EFR Home", homeUrl, true);
+    await RemoveQuickLaunchItem(webUrl, ["Pages", "Documents"]);
+}
+/**
+ *  Adds a custom webpart to the page at editformUrl
+ * 
+ * @param {string} webRelativeUrl -- The web containing the list
+ * @param {any} editformUrl -- the url of the editform page
+ * @param {string} webPartXml  -- the xml for the webpart to add
+ * @memberof EfrAdmin
+ */
+export async function AddWebPartToEditForm(webRelativeUrl: string, editformUrl, webPartXml: string) {
+    const clientContext: SP.ClientContext = new SP.ClientContext(webRelativeUrl);
+    var oFile = clientContext.get_web().getFileByServerRelativeUrl(editformUrl);
+    var limitedWebPartManager = oFile.getLimitedWebPartManager(SP.WebParts.PersonalizationScope.shared);
+    let webparts = limitedWebPartManager.get_webParts();
+    clientContext.load(webparts, 'Include(WebPart)');
+    clientContext.load(limitedWebPartManager);
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+    let originalWebPartDef = webparts.get_item(0);
+    let originalWebPart = originalWebPartDef.get_webPart();
+    originalWebPart.set_hidden(true);
+    originalWebPartDef.saveWebPartChanges();
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            console.log("the webpart was hidden");
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+    let oWebPartDefinition = limitedWebPartManager.importWebPart(webPartXml);
+    let oWebPart = oWebPartDefinition.get_webPart();
+    limitedWebPartManager.addWebPart(oWebPart, 'Main', 1);
+    clientContext.load(oWebPart);
+    await new Promise((resolve, reject) => {
+        clientContext.executeQueryAsync((x) => {
+            console.log("the new webpart was added");
+            resolve();
+        }, (error) => {
+            console.log(error);
+            reject();
+        });
+    });
+}
+export async function addCustomListWithContentType(web: Web, listTitle: string,
+    listdDescription: string, contentTypeId: string,
+    addMessage: (message: string) => void): Promise<List> {
+    let list: List;
+    await web.lists.add(listTitle, listdDescription, 100, false).then(async (listResponse: ListAddResult) => {
+        addMessage("Created List " + "Primary Approver");
+        list = listResponse.list;
+    }).catch(error => {
+        debugger;
+        console.error(error);
+        addMessage(error.data.responseBody["odata.error"].message.value);
+        return;
+    });
+    await list.contentTypes.addAvailableContentType(contentTypeId).then((ct) => {
+        addMessage("Added Primary Approver content type");
+        return;
+    }).catch(error => {
+        debugger;
+        console.error(error);
+        this.addMessage(error.data.responseBody["odata.error"].message.value);
+        return;
+    });
+    return Promise.resolve(list);
+}
+export function processUploadedFiles(httpClient: HttpClient, functionUrl: string): void {
+    debugger;
+    // call the azure function to write the message to the queue, to start the webjob to process the files
+    //https://grctest.azurewebsites.net/api/HttpTriggerCSharp1?code=HBM82bnia7nKPC/nqVTbaCmfPaFyubQa8iY22lb0r88fdQH370CRUg==&SiteType=%27Role%20to%20Tcode%20Review%27&SiteUrl=%27jwh%27&PrimaryApproverList=%27pal%27&RoleReview=%27rr%27&RoleToTransaction=%27rtt%27
+    // query param is SiteType='Role to Tcode Review' SiteUrl='url to the new web' PrimaryApproverList='name of the file we updaded to Documents'  RoleReview='name of the file we updaded to Documents' RoleToTransaction='name of the file we updaded to Documents'
+    const requestHeaders: Headers = new Headers();
+    requestHeaders.append("Content-type", "application/json");
+    requestHeaders.append("Cache-Control", "no-cache");
+
+    const postOptions: IHttpClientOptions = {
+        headers: requestHeaders,
+    };
+    httpClient.get(functionUrl, HttpClient.configurations.v1, postOptions)
+        .then((response: HttpClientResponse) => {
+            alert('Request queued');
+        })
+        .catch((error) => {
+            alert('error queuing request');
+        });
+}
--- src/webparts/highRiskAdminWebpart/components/HighRiskAdminWebpart.tsx
+++ src/webparts/highRiskAdminWebpart/components/HighRiskAdminWebpart.tsx
@@ -16,30 +16,280 @@ require('microsoft-ajax');
 require('sp-runtime');
 require('sharepoint');
 require('sp-workflow');
-import {CachedId,findId,uploadFile,esnureUsers,extractColumnHeaders} from "../../../utilities/Utilities";
-export default class HighRiskAdminWebpart extends React.Component<IHighRiskAdminWebpartProps,IHighRiskAdminWebpartState> {
+import {
+  CachedId, findId, uploadFile, esnureUsers, extractColumnHeaders, processUploadedFiles
+  , setWebToUseSharedNavigation, fixUpLeftNav, addCustomListWithContentType, cleanupHomePage
+} from "../../../utilities/Utilities";
+export default class HighRiskAdminWebpart extends React.Component<IHighRiskAdminWebpartProps, IHighRiskAdminWebpartState> {
+  cachedIds: Array<CachedId> = []
+  private reader;
+  private counter = 0;
+  constructor(props: IHighRiskAdminWebpartProps) {
+
+    super(props);
+
+
+    this.state = {
+      siteName: "",
+      newWeb: null,
+      newWebUrl: null,
+
+
+      messages: [],
+      roleToTransactionRowsUploaded: 0,
+      roleToTransactionTotalRows: 0,
+      roleToTransactionStatus: "",
+      roleToTransactionFile: null,
+
+      highRiskTotalRows: 0,
+      highRiskRowsUploaded: 0,
+      highRiskStatus: "",
+      highRiskFile: null,
+
+      primaryApproversTotalRows: 0,
+      primaryApproversRowsUploaded: 0,
+      primaryApproversStatus: "",
+      primaryApproversFile: null,
+    };
+
+
+  }
   private addMessage(message: string) {
     let messages = this.state.messages;
     var copy = map(this.state.messages, clone);
     copy.push(message);
     this.setState((current) => ({ ...current, messages: copy }));
   }
+  // #region High Risk Users
+  public async esnureHighRiskUsers(error, data: Array<any>): Promise<any> {
+    debugger;
+    this.setState((current) => ({ ...current, roleReviewStatus: "Validating users" }));
+    esnureUsers(pnp.sp.web, this.cachedIds, data, "ApproverEmail", this.addMessage);
+    this.addMessage("Done validating RoleReviewUsers");
+    this.setState((current) => ({ ...current, roleReviewStatus: "Completed Validation" }));
+    return Promise.resolve();
+  }
+  public extractColumnHeadersHighRiskUsers(headerRow: Array<String>): String[] {
+    debugger;
+    const requiredColumns = ["ApproverEmail", "AlternateApproverEmail", "Role Name"];
+    for (let requiredColumn of requiredColumns) {
+      if (headerRow.indexOf(requiredColumn) === -1) {
+        this.addMessage(`Column ${requiredColumn} is missing on Role Review Data File`);
+      }
+    }
+
+    return extractColumnHeaders(headerRow);
+
+  }
+  public onHighRiskUsersDataloaded() {
+    this.setState((current) => ({ ...current, roleReviewStatus: "Parsing file" }));
+    parse(this.reader.result, { delimiter: ',', columns: this.extractColumnHeadersHighRiskUsers }, this.esnureHighRiskUsers);
+  }
+  public parseHighRiskUsersFile(): Promise<any> {
+
+    //https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
+    //let file: File = e.target["files"][0];
+    this.setState((current) => ({ ...current, roleReviewStatus: "Reading file" }));
+    this.reader = new FileReader();
+    this.reader.onload = this.onHighRiskUsersDataloaded;
+    this.reader.readAsText(this.state.highRiskFile);
+    return Promise.resolve();
+
+  }
+  public uploadHighRiskUsersFile(): Promise<any> {
+    debugger;
+    this.setState((current) => ({ ...current, roleReviewStatus: "Uploading file" }));
+    return uploadFile(this.state.newWeb, "Documents", this.state.highRiskFile, "High Risk", this.addMessage);
+
+  }
+  public saveHighRiskFile(e: any) {
+
+    //https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
+    let file: File = e.target["files"][0];
+    this.setState((current) => ({ ...current, highRiskFile: file }));
+  }
+  public async createSite() {
+    let newWeb: Web;  // the web that gets created
+    let libraryList: Array<any>; // the list of libraries we need to create in the new site. has the library name and the name of the group that should get access
+    let foldersList: Array<string>; // the list of folders to create in each of the libraries.
+    let roleDefinitions: Array<any>;// the roledefs for the site, we need to grant 'contribute no delete'
+    let siteGroups: Array<any>;// all the sitegroups in the site
+    let tasks: Array<any>; // the list of tasks in the TaskMaster list. We need to create on e task for each of these in tye EFRTasks list in the new site
+    let taskList: List; // the task list we created  in the new site
+    let taskListId: string; // the ID of task list we created  in the new site
+    let webServerRelativeUrl: string; // the url of the subweb
+    let contextInfo: ContextInfo;
+    let editformurl: string;
+
+
+
+    this.addMessage("CreatingSite");
+    await pnp.sp.site.getContextInfo().then((context: ContextInfo) => {
+      contextInfo = context;
+    });
+    // create the site
+    await pnp.sp.web.webs.add(this.state.siteName, this.state.siteName, this.state.siteName, this.props.templateName).then((war: WebAddResult) => {
+      this.addMessage("CreatedSite");
+      // show the response from the server when adding the web
+      webServerRelativeUrl = war.data.ServerRelativeUrl;
+      console.log(war.data);
+      newWeb = war.web;
+      this.setState((current) => ({
+        ...current,
+        newWeb: newWeb,
+        newWebUrl: webServerRelativeUrl
+      })); /// save in state so file uploads can use it
+
+      return;
+    }).catch(error => {
+      debugger;
+      this.addMessage("<h1>error creating site</h1>");
+      this.addMessage(error.data.responseBody["odata.error"].message.value);
+      console.error(error);
+      return;
+    });
+
+    await setWebToUseSharedNavigation(webServerRelativeUrl);
+    debugger;
+    await fixUpLeftNav(webServerRelativeUrl, this.props.siteUrl);
+
+    // create the lists and assign permissions
+    let highRiskList: PNPList = await addCustomListWithContentType(newWeb, 'High Risk', "High Risk Transactions"
+      , this.props.highRiskContentTypeId, this.addMessage);
+
+    let primaryAppproversList: PNPList = await addCustomListWithContentType(newWeb, 'Primary Approvers', "Primary Approvers of High Risk Transactions"
+      , this.props.primaryApproverContentTypeId, this.addMessage);
+
+    let roleToTransactionList: PNPList = await addCustomListWithContentType(newWeb, 'Role To Transaction', "Role To Transaction DETAILS"
+      , this.props.roleToTransactionContentTypeId, this.addMessage);
+    await roleToTransactionList.fields.getByInternalNameOrTitle("GRCRoleName").update({
+      Indexed: true
+    }).then((resp) => {
+      debugger;
+    }).catch((error) => {
+      debugger;
+      console.error(error);
+      this.addMessage(error.data.responseBody["odata.error"].message.value);
+      return;
+
+    });
+    // this is a list that the webjob can use to log messages
+    let messageList: PNPList = await addCustomListWithContentType(newWeb, 'Messages', "Messages from the provsioning Web Job"
+      , "0x01", this.addMessage);
+
+    // customize the home paged
+    let welcomePageUrl: string;
+    await newWeb.rootFolder.getAs<any>().then(rootFolder => {
+      debugger;
+      welcomePageUrl = rootFolder.ServerRelativeUrl + rootFolder.WelcomePage;
+    });
+    this.addMessage("Customizing Home Page");
+    await cleanupHomePage(webServerRelativeUrl, welcomePageUrl, this.props.webPartXml);
+    this.addMessage("Customized Home Page");
+
+    this.addMessage("DONE!!");
+
+  }
+  private processUploadedFiles(): void {
+    debugger;
+
+    let functionUrl = `${this.props.azureFunctionUrl}
+       &siteUrl=${this.props.siteAbsoluteUrl + "/" + this.state.siteName}
+       &siteType=Role To Ttansaction
+       &PrimaryApproverList=Primary Approvers List
+       &RoleReview=Role Review
+       &RoleToTransaction=Role To Transaction`
+    processUploadedFiles(this.props.httpClient, functionUrl);
+  }
   public render(): React.ReactElement<IHighRiskAdminWebpartProps> {
+
     return (
-      <div className={ styles.highRiskAdminWebpart }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
+      <div className={styles.grcUpload} >
+
+
+        <table>
+          <thead>
+            <th>
+              SharePoint List
+          </th>
+            <th>
+              File
+          </th>
+            <th>
+              Status
+          </th>
+            <th>
+              Total Rows
+          </th>
+            <th>
+              Rows Uploaded
+          </th>
+
+          </thead>
+          <tr>
+            <td>
+              Role Review
+            </td>
+            <td>
+              <input type="file" id="uploadrttfile" onChange={e => { this.saveHighRiskFile(e); }} />
+            </td>
+            <td>
+              {this.state.highRiskStatus}
+            </td>
+            <td>
+              {this.state.highRiskTotalRows}
+            </td>
+            <td>
+              {this.state.highRiskRowsUploaded}
+            </td>
+            <td>
+              <IconButton iconProps={{ iconName: "DocumentApproval" }} onClick={this.parseHighRiskUsersFile}>Upload</IconButton>
+              <IconButton iconProps={{ iconName: "Save" }} onClick={this.uploadHighRiskUsersFile}>Upload</IconButton>
+            </td>
+
+          </tr>
+
+        </table>
+        <hr />
+        <table>
+          <tr>
+            <td>
+              New Site Name
+            </td>
+            <td>
+              <TextField label="" onChanged={(e) => {
+                this.setState((current) => ({ ...current, siteName: e }));
+              }} />
+            </td>
+            <td>
+              <PrimaryButton onClick={this.createSite.bind(this)} title="Create Site">Create Site</PrimaryButton>
+            </td>
+            <td>
+              Active Site:
+            </td>
+            <td>
+              {this.state.newWebUrl}
+            </td>
+
+          </tr>
+
+        </table>
+        <button onClick={this.processUploadedFiles}>Process Uploaded Files</button>
+        <div style={{ border: '1px', borderStyle: "solid" }} >
+          <IconButton iconProps={{ iconName: "Clear" }}
+            onClick={
+              () => { this.setState((current) => ({ ...current, messages: [] })); }
+            }>Upload</IconButton>
+          <List items={this.state.messages}
+            onRenderCell={(item: any, index: number, isScrolling: boolean) => {
+              return (
+                <div>{item}</div>
+              );
+            }
+            } />
+
         </div>
-      </div>
+      </div >
     );
   }
 }
